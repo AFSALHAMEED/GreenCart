4ac0ea597d8a948fdfc9acf3dcd2c88dc94a1744
afsal.vh <afsalafsu821@gmail.com>
2025-04-27 06:39:17 +0530

    re named api end point

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(cors({ origin: allowedOrigin, credentials: true }));
 
 app.post(
-  "/stripe",
+  "/stripe/webhook",
   bodyParser.raw({ type: "application/json" }),
   stripWebhooks
 );
